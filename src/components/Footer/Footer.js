fc27e531a09bb7e3d3573234e6e0e1414177dce1
FCraven <craven.francis@gmail.com>
2019-03-16 23:18:29 -0400

    added Footer component for social media

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer =()=> {
+  return (
+    <div className='footer container container-fluid'>
+
+      <footer class="footer">
+        <div class="container text-center">
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-linkedin"></i></a>
+          <a href="#"><i class="fa fa-google-plus"></i></a>
+          <a href="#"><i class="fa fa-skype"></i></a>
+        </div>
+      </footer>
+
+    </div>
+  )
+}
+
+export default Footer
