c7f36de7dd3577a7deec95eb599f72fe2d2c1d9a
FCraven <craven.francis@gmail.com>
2019-03-23 20:31:21 -0400

    didnt work

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -7,19 +7,19 @@ export default class Home extends Component {
     super(props)
 
     this.state = {
-        pictures: ['/moustacheSelfie.JPG','/skylineSelfie.jpeg','/twinningSelfie.jpeg']
+      pictures: ['/moustacheSelfie.JPG', '/skylineSelfie.jpeg', '/twinningSelfie.jpeg']
     }
   }
 
   render() {
     const pictures = this.state.pictures;
-    const picture = pictures[Math.floor(Math.random()*pictures.length)]
+    const picture = pictures[Math.floor(Math.random() * pictures.length)]
     return (
 
-      <div class="hero-image container container-fluid animated fadeOut">
+      <div class="hero-image container container-fluid">
         <div class="hero-text">
 
-          <img src={picture} className='rounded-circle img-fluid hero-picture border border-light' alt='me'/>
+          <img src={picture} className='rounded-circle img-fluid hero-picture border border-light' alt='me' />
           <div className='typed-stuff pt-4'>
             <p className='text-light h4'> Hi! My name is Frank.</p>
             <p className='text-light h4'>I'm a Full Stack Developer</p>
