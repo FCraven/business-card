2db84f1d60c92479181179bdfee5c4ce976d3317
FCraven <craven.francis@gmail.com>
2019-03-27 21:57:18 -0400

    more responsive issues fixed

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -18,31 +18,30 @@ export default class Home extends Component {
     return (
 
       <div className="hero-image container container-fluid">
-        <div className="hero-text ">
+        <div className="hero-text">
 
-
-            <div className='typed-stuff pt-4 ml-auto'>
               <Typing startDelay={2500} speed={75}>
-                <p className='text-light h4'> Hi! <Typing.Delay ms={1000} /> My name is Frank.</p>
+                <p className='text-light h4 p-1'> Hi! <Typing.Delay ms={1000} /> My name is Frank.</p>
                 <p className='text-light h4'>
                   <Typing.Delay ms={1000}/>
-                      I'm a front end developer.
-                      <Typing.Delay ms={750} />
-                      <Typing.Backspace count={20}/>
-                      back end developer.
-                      <Typing.Delay ms={750} />
-                      <Typing.Backspace count={19}/>
+                      I'm a front end
+                      <Typing.Delay ms={1000} />
+                      <Typing.Backspace count={9}/>
+                      back end
+                      <Typing.Delay ms={1000} />
+                      <Typing.Backspace count={8}/>
                       Full Stack Software Engineer
                 </p>
               </Typing>
-            </div>
 
             <img src={picture} className='rounded-circle m-2 img-fluid hero-picture border border-light' alt='me' />
 
           <hr className='bg-light'style={{width:'50%'}}/>
 
           <NavLink to='/menu'>
-            <button className='btn btn-outline-light learn-more-button'> Learn More About Me </button>
+            <button className='btn btn-large bg-dark text-light learn-more-button'>
+              <b>Learn More About Me</b>
+            </button>
           </NavLink>
         </div>
       </div>
