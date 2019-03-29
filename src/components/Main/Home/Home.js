7067cf9d4639f78cc6d14ac21d9c79a4e37d52e4
FCraven <craven.francis@gmail.com>
2019-03-29 11:56:27 -0400

    removed image from home screen

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -1,27 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Typing from 'react-typing-animation'
 import { NavLink } from 'react-router-dom'
 import './Home.css'
 
-export default class Home extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      pictures: ['/moustacheSelfie.JPG', '/skylineSelfie.jpeg', '/twinningSelfie.jpeg']
-    }
-  }
-
-  render() {
-    const pictures = this.state.pictures;
-    const picture = pictures[Math.floor(Math.random() * pictures.length)]
+const Home =()=> {
     return (
-
       <div className="hero-image container container-fluid">
         <div className="hero-text">
 
               <Typing startDelay={2500} speed={75}>
-                <p className='text-light h4 p-1'> Hi! <Typing.Delay ms={1000} /> My name is Frank.</p>
+              <p className='text-light h4 p-1'> Hi!</p>
+                <p className='text-light h4 p-1'><Typing.Delay ms={1000} /> My name is Frank.</p>
                 <p className='text-light h4'>
                   <Typing.Delay ms={1000}/>
                       I'm a front end
@@ -34,8 +23,6 @@ export default class Home extends Component {
                 </p>
               </Typing>
 
-            <img src={picture} className='rounded-circle m-2 img-fluid hero-picture border border-light' alt='me' />
-
           <hr className='bg-light'style={{width:'50%'}}/>
 
           <NavLink to='/menu'>
@@ -45,9 +32,8 @@ export default class Home extends Component {
           </NavLink>
         </div>
       </div>
-
-
     )
   }
-}
+
+  export default Home
 
