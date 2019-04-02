ac58d17bc47d1fdb40d14a288a13beae5fc1be38
FCraven <craven.francis@gmail.com>
2019-04-01 20:37:57 -0400

    attempting to add dropdowns to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -106,6 +106,21 @@ export default class Navbar extends Component {
                 </NavLink>
               </li>
 
+              <li className='nav-item'>
+
+                <div class="dropdown">
+                  <button class="btn btn-primary dropdown-toggle"
+                          type="button"
+                          data-toggle="dropdown">Dropdown Example
+                  <span class="caret"></span></button>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">HTML</a></li>
+                    <li><a href="#">CSS</a></li>
+                    <li><a href="#">JavaScript</a></li>
+                  </ul>
+                </div>
+              </li>
+
             </ul>
           </div>
         </nav>
