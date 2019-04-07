d1635a4c7e326c5638bdbb99dfd0c03aa008174c
FCraven <craven.francis@gmail.com>
2019-04-07 15:44:46 -0400

    carousel is working, trying to adjust background image fill screen and be responsive

--- src/components/Main/TechStack/TechStack.js
+++ src/components/Main/TechStack/TechStack.js
@@ -4,30 +4,48 @@ import './TechStack.css'
 export default class TechStack extends Component {
   render() {
     return (
-        <div id="carouselExampleIndicators" class="carousel slide pt-5" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+
+        <div id="techStackCarousel" className="carousel slide" data-ride="carousel">
+
+          <ol className="carousel-indicators ">
+
+            <li data-target="#techStackCarousel" data-slide-to="0" className="active"></li>
+            <li data-target="#techStackCarousel" data-slide-to="1"></li>
+            <li data-target="#techStackCarousel" data-slide-to="2"></li>
+
           </ol>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="d-block w-100" src="..." alt="First slide" />
+
+          <div className="carousel-inner">
+
+            <div className="carousel-item active bg-primary h-100 w-100">
+{/*
+              <img  className="d-block w-100 "
+                    src="https://cdn.acodez.in/wp-content/uploads/2017/11/Javascript-Front-End-Development-Frameworks.jpg"
+                    alt="Client Side"
+                    /> */}
+               
+              <div className='carousel-caption'>
+                <h2 className='text-white'><b>Client Side</b></h2>
+                <p className='text-white text-center'>Check out how I develop the Front End</p>
+              </div>
             </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="..." alt="Second slide" />
+
+            <div className="carousel-item">
+              <img className="d-block w-100" src="..." alt="Second slide" />
             </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="..." alt="Third slide" />
+
+            <div className="carousel-item">
+              <img className="d-block w-100" src="..." alt="Third slide" />
             </div>
           </div>
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
+
+          <a className="carousel-control-prev" href="#techStackCarousel" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
           </a>
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
+          <a className="carousel-control-next" href="#techStackCarousel" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
           </a>
         </div>
     )
