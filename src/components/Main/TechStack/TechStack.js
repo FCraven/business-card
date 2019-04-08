f5724c1770cecfb0e3de2c7a1339e2fea29c93fb
FCraven <craven.francis@gmail.com>
2019-04-08 19:44:10 -0400

    carousel removed and added image cards with text overlay. Carousel seemed good but was not the right option

--- src/components/Main/TechStack/TechStack.js
+++ src/components/Main/TechStack/TechStack.js
@@ -3,51 +3,18 @@ import './TechStack.css'
 
 export default class TechStack extends Component {
   render() {
+
     return (
-
-        <div id="techStackCarousel" className="carousel slide" data-ride="carousel">
-
-          <ol className="carousel-indicators ">
-
-            <li data-target="#techStackCarousel" data-slide-to="0" className="active"></li>
-            <li data-target="#techStackCarousel" data-slide-to="1"></li>
-            <li data-target="#techStackCarousel" data-slide-to="2"></li>
-
-          </ol>
-
-          <div className="carousel-inner">
-
-            <div className="carousel-item active bg-primary h-100 w-100">
-{/*
-              <img  className="d-block w-100 "
-                    src="https://cdn.acodez.in/wp-content/uploads/2017/11/Javascript-Front-End-Development-Frameworks.jpg"
-                    alt="Client Side"
-                    /> */}
-               
-              <div className='carousel-caption'>
-                <h2 className='text-white'><b>Client Side</b></h2>
-                <p className='text-white text-center'>Check out how I develop the Front End</p>
-              </div>
-            </div>
-
-            <div className="carousel-item">
-              <img className="d-block w-100" src="..." alt="Second slide" />
-            </div>
-
-            <div className="carousel-item">
-              <img className="d-block w-100" src="..." alt="Third slide" />
-            </div>
+<div className='container container-fluid'>
+        <div className="card bg-dark text-white stack-card mx-auto">
+          <img className="card-img img-fluid" style={{maxHeight:'75vh'}} src="https://c.pxhere.com/photos/53/bb/apple_business_computer_desk_imac_ipad_keyboard_laptop-911811.jpg!d" alt="Front End Stack" />
+          <div className="card-img-overlay">
+            <h5 className="card-title">Client Side</h5>
+            <p className="card-text">See what languages and libraries I use to develop the Front End</p>
+            <p className="card-text"> Front End </p>
           </div>
-
-          <a className="carousel-control-prev" href="#techStackCarousel" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="#techStackCarousel" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
         </div>
+</div>
     )
   }
 }
