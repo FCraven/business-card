fb020dc5099b519e893615ef9f4b0ac95a95a777
FCraven <craven.francis@gmail.com>
2019-04-11 17:41:43 -0400

    added escape button to Other Tech component while I work on it

--- src/components/Main/TechStack/OtherTech/OtherTech.js
+++ src/components/Main/TechStack/OtherTech/OtherTech.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const OtherTech =(props)=> {
   return (
     <div className="container">
-      <div className="jumbotron p">
+      <div className="jumbotron  text-center mt-5 display-4">
         Currently under construction, check back soon!
+      <Link to='/techstack'>
+        <button className='btn btn-large btn-primary'>Back to<br/>TechStack</button>
+      </Link>
       </div>
     </div>
   )
