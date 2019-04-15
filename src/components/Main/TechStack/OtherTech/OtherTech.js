39ef7943d5808bdcc6d4037506e11646cdcdf558
FCraven <craven.francis@gmail.com>
2019-04-15 19:57:55 -0400

    added todo in OtherTech

--- src/components/Main/TechStack/OtherTech/OtherTech.js
+++ src/components/Main/TechStack/OtherTech/OtherTech.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const OtherTech =(props)=> {
+//add compilers, bundlers, all fthe other stuff from your resume
+
+const OtherTech = (props) => {
   return (
     <div className="container">
       <div className="jumbotron  text-center mt-5 display-4">
         Currently under construction, check back soon!
       <Link to='/techstack'>
-        <button className='btn btn-large btn-primary'>Back to<br/>TechStack</button>
-      </Link>
+          <button className='btn btn-large btn-primary'>Back to<br />TechStack</button>
+        </Link>
       </div>
     </div>
   )
